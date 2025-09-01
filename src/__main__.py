ce39b93f4eb73a4ecd2d534d0eb3877b729b7c4e
Alvarez Romain <roalvare@student.42.fr>
2025-09-01 21:56:29 +0200

    feat: add llm_sdk local module. First call test

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,11 @@
+from .llm_sdk import Small_LLM_Model
+
 def main():
     print("Hello from ai-beta-llm!")
+
+    model = Small_LLM_Model()
+    path = model.get_path_to_vocabulary_json()
+    print(f"vocabulary path: {path}")
 
 
 if __name__ == "__main__":
